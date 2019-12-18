9bc8f422dc058309afb04e79308cd24d85650000
Celcial <DavidGray@Outlook.co.nz>
2019-12-19 08:54:55 +1300

    Advanced Topics setup

---
+++ AdvancedTopics/AdvancedTopics/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AdvancedTopics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
